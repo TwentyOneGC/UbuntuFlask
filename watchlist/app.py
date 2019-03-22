7ad224202b220c897375b10e4bee0e8e541be23c
Skey <931086574@qq.com>
2019-03-22 15:31:19 +0800

    Add database

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,25 +1,69 @@
 from flask import Flask
 from flask import url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os, sys
+import click
+
+
+WIN = sys.platform.startswith('win')
+if WIN:     # windows: use '///'
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
-name = 'Skey'
-movies = [
-    {'title':'My Neighbor Totoro', 'year':'1988'},
-    {'title':'Dead Poets Society', 'year':'1989'},
-    {'title':'A Perfect World', 'year':'1993'},
-    {'title':'Leon', 'year':'1994'},
-    {'title':'Mahjong', 'year':'1996'},
-    {'title':'Smallowtail Butterfly', 'year':'1996'},
-    {'title':'King of Comedy', 'year':'1999'},
-    {'title':'Devils on the Doorstep', 'year':'1999'},
-    {'title':'WALL-E', 'year':'2008'},
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # Turn off monitoring of model changes
+db = SQLAlchemy(app)    # init extension
+@app.cli.command()  # register command
+@click.option('--drop', is_flag=True, help='Creat after drop')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database')
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Skey'
+    movies = [
+        {'title':'My Neighbor Totoro', 'year':'1988'},
+        {'title':'Dead Poets Society', 'year':'1989'},
+        {'title':'A Perfect World', 'year':'1993'},
+        {'title':'Leon', 'year':'1994'},
+        {'title':'Mahjong', 'year':'1996'},
+        {'title':'Smallowtail Butterfly', 'year':'1996'},
+        {'title':'King of Comedy', 'year':'1999'},
+        {'title':'Devils on the Doorstep', 'year':'1999'},
+        {'title':'WALL-E', 'year':'2008'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Data generation complete')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(20))
 
 @app.route('/') # Similar to the Django view
 def index():
     # return  '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', name=user.name, movies=movies)
 
+
+
+'''
 @app.route('/user/<name>')
 def user_page(name):
     return "User: %s" % name
@@ -31,6 +75,8 @@ def test_url_for():
     print(url_for('test_url_for')) # /test
     print(url_for('test_url_for', num=2))   # /test?num=2
     return 'Test Page'
+    
+'''
 
 if __name__ == '__main__':
     app.run()
