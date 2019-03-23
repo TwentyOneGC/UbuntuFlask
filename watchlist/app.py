7ef07b898f43cfdd36f62f814391057fc8468d90
Skey <931086574@qq.com>
2019-03-23 10:57:37 +0800

    Create Del Add for form

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import url_for, render_template
+from flask import url_for, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
 import click
@@ -14,6 +14,7 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # Turn off monitoring of model changes
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)    # init extension
 
 @app.cli.command()  # register command
@@ -60,9 +61,18 @@ def inject_user():
     user = User.query.first()
     return dict(user=user)
 
-@app.route('/') # Similar to the Django view
+@app.route('/', methods=['GET','POST']) # Similar to the Django view
 def index():
-    # return  '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(title) > 60 or len(year) > 4 :
+            flash('Invalid input')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Successful')
     # user = User.query.first()
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
@@ -72,21 +82,60 @@ def page_not_found(e):
     # user = User.query.first()
     return render_template('404.html'), 404
 
+@app.route('/movie/edit/<int:movie_id>', methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(title) > 60 or len(year) > 4 :
+            flash('Invalid input')
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Successful')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
 
-'''
-@app.route('/user/<name>')
-def user_page(name):
-    return "User: %s" % name
+@app.route('/movie/delete/<int:movie_id>', methods=['POST']) # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello')) # /
-    print(url_for('user_page', name='skey')) # user_page/skey
-    print(url_for('test_url_for')) # /test
-    print(url_for('test_url_for', num=2))   # /test?num=2
-    return 'Test Page'
-    
-'''
+# @app.route('/add', methods=['GET','POST'])
+# def add():
+#     if request.method == 'POST':
+#         title = request.form.get('title')
+#         year = request.form.get('year')
+#         if not title or not year or len(year) > 4 or len(title) > 60 :
+#             flash('Invalid input')
+#             return redirect(url_for('index'))
+#         movie = Movie(title=title, year=year)
+#         db.session.add(movie)
+#         db.session.commit()
+#         flash('Item created')
+#         return redirect(url_for('index'))
+#     movies = Movie.query.all()
+#     return render_template('add.html',movies=movies)
 
+
+
+
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return "User: %s" % name
+#
+# @app.route('/test')
+# def test_url_for():
+#     print(url_for('hello')) # /
+#     print(url_for('user_page', name='skey')) # user_page/skey
+#     print(url_for('test_url_for')) # /test
+#     print(url_for('test_url_for', num=2))   # /test?num=2
+#     return 'Test Page'
+#
 if __name__ == '__main__':
     app.run()
